7d25ddb9eb26339e42bcb62860c0ea925b4d4cc8
MuraD603 <msafarov603@gmail.com>
2025-03-27 22:47:26 +0300

    Исправлен код

--- task1/task1.py
+++ task1/task1.py
@@ -1,29 +1,39 @@
 import sys
 
-def circular_path(n, m):
-    arr = list(range(1, n + 1))
-    path = []
-    current = 0
-    
-    while True:
-        path.append(str(arr[current]))
-        current = (current + m - 1) % n
-        if current == 0:
-            break
-    return ''.join(path)
-
-if __name__ == "__main__":
-    # Проверка
+def main():
     if len(sys.argv) != 3:
         print("Ошибка: требуется 2 аргумента — n и m")
-        print("Пример: python task1/task1.py 4 3")
+        print("Пример: python task.py 5 4")
         sys.exit(1)
     
     try:
         n = int(sys.argv[1])
         m = int(sys.argv[2])
+        
+        if n <= 0 or m <= 0:
+            print("Ошибка: n и m должны быть положительными числами.")
+            sys.exit(1)
+        
+        print(circular_path(n, m))
     except ValueError:
-        print("Ошибка: аргументы должны быть целыми числами")
+        print("Ошибка: Введите целые числа.")
         sys.exit(1)
+
+def circular_path(n, m):
+    arr = list(range(1, n + 1))
+    path = []
+    current = 0
     
-    print(circular_path(n, m))
+    visited = set()
+    
+    while current not in visited:
+        path.append(str(arr[current]))
+        visited.add(current)
+        current = (current + m - 1) % n
+    
+    return ''.join(path)
+
+
+if __name__ == "__main__":
+    main()
+
